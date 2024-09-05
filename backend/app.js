4039f232bc761c004589e5f46f38b5962c1a12c6
RAJ VARDHAN CHAUDHARY <112545110+rajchaudhary99@users.noreply.github.com>
2024-09-05 23:37:21 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const productRoutes = require('./routes/cards');
+
+// Create an Express application
+const app = express();
+
+// Set up CORS
+app.use(cors({
+  origin: ['https://deploy.mern.1whq.vercel.app'],
+  methods: ['POST', 'GET'],
+  credentials: true
+}));
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// MongoDB connection
+const DB = 'mongodb+srv://rajvardhanchaudhary666:Xq2k7HKxHMyr3qK2@cluster0.aywxj.mongodb.net/Help-Center';
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// API Routes
+app.use('/api/products', productRoutes);
+
+// Export the app for use in server.js
+module.exports = app;
